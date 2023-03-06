b98d54c5ba24391e607c7de2b69d9af73cd7b150
kuxaden <kuxade413@gmail.com>
2023-03-06 16:32:11 +0200

    v.1.0

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+const { active } = require("browser-sync");
+const { byteLength } = require("engine.io-client/build/esm/util");
+
+
+function openMenu() {
+   document.querySelector(".menu-list").classList.toggle('active');
+   document.querySelector(".navbar-btn").classList.toggle('active');
+};
+function closeMenu() {
+   document.querySelector(".menu-list").classList.toggle('active');
+   document.querySelector(".navbar-btn").classList.toggle('active');
+};
+
+function startAnimate() {
+   document.querySelector(".about").classList.add('animated');
+   var items = document.querySelectorAll(".about-block__text-item");
+   for (var i = 0; i < items.length; ++i) {
+      items[i].classList.add('animated');
+   }
+};
+
