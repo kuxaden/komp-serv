4d01346485b72da6163cd0c7d98a9e2b029d0e1e
kuxaden <kuxade413@gmail.com>
2023-03-13 18:47:39 +0200

    v.1.12

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,52 @@ function closeMenu() {
 
 };
 
+// скрыть - меню навигации при прокрутке вниз
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+   closeMenu();
+   var currentScrollPos = window.pageYOffset;
+   if (prevScrollpos > currentScrollPos) {
+      document.querySelector(".header").style.top = "0";
+   } else {
+      document.querySelector(".header").style.top = "-80px";
+   }
+   prevScrollpos = currentScrollPos;
+}
+
+// Рандомно перемешивает элементы блока
+const carusel = document.querySelector('.services-block');
+carusel.innerHTMl = '';
+[...carusel.children]
+   .sort(() => Math.random() - 0.5)
+   .forEach(v => carusel.append(v));
+
+
+// Проверяет поддерживает ли браузер касание
+if ('ontouchstart' in window) {
+   console.log("/* browser with Touch Events support */");
+} else {
+   console.log("/* browser with Touch Events NO support */");
+};
+// Создаёт событие которое отслеживает касание на объекте about
+document.addEventListener("DOMContentLoaded", startup);
+function startup() {
+   var el = document.querySelector(".about");
+   el.addEventListener("touchstart", handleStart, false);
+}
+// в случае срабатывания запускает функцию StartAnimate()
+function handleStart(evt) {
+   console.log("startAnimate");
+   startAnimate();
+}
+
 function startAnimate() {
    document.querySelector(".about").classList.add('animated');
    var items = document.querySelectorAll(".about-block__text-item");
    for (var i = 0; i < items.length; ++i) {
       items[i].classList.add('animated');
-   }
+   };
+   closeMenu();
 };
 
 // Задержка  перед выполнением ссылки в одну секунду.
@@ -22,17 +62,17 @@ window.onload = function () {
    var links = document.querySelectorAll('a');
 
    links.forEach(function (link) {
-     link.addEventListener('click', onLinkClicked);
+      link.addEventListener('click', onLinkClicked);
 
-     function onLinkClicked(event) {
-       event.preventDefault();
-       setTimeout(onAnimationComplete, 1000);
-     }
+      function onLinkClicked(event) {
+         event.preventDefault();
+         setTimeout(onAnimationComplete, 1000);
+      }
 
-     function onAnimationComplete() {
-       window.location = link.href;
-     }
+      function onAnimationComplete() {
+         window.location = link.href;
+      }
    });
-   
- }
+
+}
 
